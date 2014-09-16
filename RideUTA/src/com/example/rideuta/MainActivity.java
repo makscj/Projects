1fd514b27750b4c85c420303d5c33608bc8c9dbf
maksus01 <maksus01@hotmail.com>
2014-09-15 21:03:42 -0600

    playing around with loops

--- RideUTA/src/com/example/rideuta/MainActivity.java
+++ RideUTA/src/com/example/rideuta/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.rideuta;
 
-import com.example.myfirstapp.R;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.telephony.SmsManager;
@@ -10,45 +12,74 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.myfirstapp.R;
 
 public class MainActivity extends ActionBarActivity {
 	
-	private Button pres, web, home220, home213, libr;
+//	private Button pres, web, home220, home213, libr;
 	private final String phoneNumber = "882-882";
+	
+	private HashMap<String, String> map;
+	private HashMap<Integer,String> buttonIDs;
+	private ArrayList<Button> buttons;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		pres = (Button) findViewById(R.id.pres);
-		web = (Button) findViewById(R.id.web);
-		home220 = (Button) findViewById(R.id.home220);
-		home213 = (Button) findViewById(R.id.home213);
-		libr = (Button) findViewById(R.id.libr);
+		map = new HashMap<String,String>();
+		buttonIDs = new HashMap<Integer,String>();
+		buttons = new ArrayList<Button>();
+		
+		String[] nums = {"126556","101936","126581","137240","127117"};
+		String[] names = {"pres","web","home220","home213","libr"};
+		int[] ids = {R.id.pres, R.id.web,R.id.home220,R.id.home213,R.id.libr};
+		
+		for(int i = 0; i<nums.length; i++){
+			map.put(nums[i], names[i]);
+			buttonIDs.put(ids[i], nums[i]);
+			Button b = (Button) findViewById(ids[i]);
+			buttons.add(b);
+		}
+		
+//		pres = (Button) findViewById(R.id.pres);
+//		web = (Button) findViewById(R.id.web);
+//		home220 = (Button) findViewById(R.id.home220);
+//		home213 = (Button) findViewById(R.id.home213);
+//		libr = (Button) findViewById(R.id.libr);
 	}
 	public void sendMessage(View v){
 		String message;
-		switch(v.getId()){
-		case R.id.pres:
-			message = "126556";
-			sendSMS(phoneNumber, message);
-			break;
-		case R.id.web:
-			message = "101936";
-			sendSMS(phoneNumber, message);
-			break;
-		case R.id.home220:
-			message = "126581";
-			sendSMS(phoneNumber, message);
-			break;
-		case R.id.home213:
-			message = "137240";
-			sendSMS(phoneNumber, message);
-			break;
-		case R.id.libr:
-			message = "127117";
-			sendSMS(phoneNumber, message);
-			break;
+		for(Button b : buttons){
+			if(b.getId()==v.getId()){
+				message = buttonIDs.get(v.getId());
+				sendSMS(phoneNumber,message);
+				break;
+			}
 		}
+//		switch(v.getId()){
+//		case R.id.pres:
+//			message = "126556";
+//			sendSMS(phoneNumber, message);
+//			break;
+//		case R.id.web:
+//			message = "101936";
+//			sendSMS(phoneNumber, message);
+//			break;
+//		case R.id.home220:
+//			message = "126581";
+//			sendSMS(phoneNumber, message);
+//			break;
+//		case R.id.home213:
+//			message = "137240";
+//			sendSMS(phoneNumber, message);
+//			break;
+//		case R.id.libr:
+//			message = "127117";
+//			sendSMS(phoneNumber, message);
+//			break;
+//		}
 	}
 	private void sendSMS(String phoneNumber, String message){
 		Log.v("phoneNumber",phoneNumber);
@@ -58,7 +89,11 @@ public class MainActivity extends ActionBarActivity {
 	}
 	
 	public void addNumber(View v){
+		EditText edit = (EditText)findViewById(R.id.edit_text);
+		String num = edit.getText().toString();
 		
+		
+		//second edit text
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- RideUTA/src/com/example/rideuta/Storage.java
+++ RideUTA/src/com/example/rideuta/Storage.java
@@ -1,10 +1,15 @@
 package com.example.rideuta;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+import android.widget.Button;
 
 public class Storage {
 
 	private HashMap<String, String> map;
 	
+	private ArrayList<Button> buttons;
 	
 	public Storage(){
 		map = new HashMap<String,String>();
